b42be8fe6b55c0c01b989849e07608166b31deeb
modidokeita <modibokeita9390gmail.com>
2024-04-18 13:37:33 -0700

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""
+  script that starts a Flask web application
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/',strict_slashes=False)
+def hello_hbnb():
+    """
+     returns Hello HBNB!
+    """
+    return 'Hello HBNB!'
+
+@app.route('/hbnb',strict_slashes=False)
+def hbnb():
+    """
+     returns  HBNB!
+    """
+    return 'HBNB!'
+
+@app.route('/c/<text>',strict_slashes=False)
+def cisfun(text):
+    """
+     returns  “C ” followed by the value of the text variable!
+     variable
+    """
+    return 'C ' + text.replace('_', ' ')
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>',strict_slashes=False)
+def print_python(text):
+    """
+     returns  “python ” followed by the value of the text variable!
+     variable
+    """
+    return 'Python ' + text.replace('_', ' ')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
