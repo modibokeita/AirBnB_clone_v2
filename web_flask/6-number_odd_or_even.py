f558262c118bafd81a1a2f87e58988aaadda2242
modidokeita <modibokeita9390gmail.com>
2024-04-19 15:29:34 -0700

    6-number_odd_or_even.py

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -8,21 +8,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/',strict_slashes=False)
+
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
      returns Hello HBNB!
     """
     return 'Hello HBNB!'
 
-@app.route('/hbnb',strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
      returns  HBNB!
     """
     return 'HBNB!'
 
-@app.route('/c/<text>',strict_slashes=False)
+
+@app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """
      returns  “C ” followed by the value of the text variable!
@@ -30,8 +33,9 @@ def cisfun(text):
     """
     return 'C ' + text.replace('_', ' ')
 
+
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>',strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def print_python(text):
     """
      returns  “python ” followed by the value of the text variable!
@@ -40,12 +44,13 @@ def print_python(text):
     return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<int:n>',strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def print_n(n):
     """
       display “n is a number” only if n is an integer
     """
     return "{:d} is a number".format(n)
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_templete(n):
@@ -62,6 +67,7 @@ def display_even(n):
         even_num = 'odd'
     return render_template('6-number_odd_or_even.html', n=n,
                            even_num=even_num)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
