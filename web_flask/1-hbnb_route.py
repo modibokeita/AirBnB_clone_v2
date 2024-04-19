766ced9c27d8425cddd3daaaa0f3988d7904c121
modidokeita <modibokeita9390gmail.com>
2024-04-19 15:08:35 -0700

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -4,9 +4,12 @@
   script that starts a Flask web application
 """
 
+
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/',strict_slashes=False)
 def hello_hbnb():
@@ -15,12 +18,14 @@ def hello_hbnb():
     """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb',strict_slashes=False)
 def hbnb():
     """
      returns  HBNB!
     """
     return 'HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000') 
