a3b9406d5cb636cbb0b7d743ffa583f017021a8b
modidokeita <modibokeita9390gmail.com>
2024-04-19 15:06:43 -0700

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,9 +4,12 @@
 Script that starts a Flask web application
 """
 
+
 from flask import Flask
 
+
 app = Flask(__name__)
+
 
 @app.route('/',strict_slashes=False)
 def hello_hbnb():
@@ -14,6 +17,7 @@ def hello_hbnb():
      returns Hello HBNB!
     """
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000') 
