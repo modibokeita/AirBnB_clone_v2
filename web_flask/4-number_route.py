1f5dd592f037585da86a95592502df620aa1fc86
modidokeita <modibokeita9390gmail.com>
2024-04-19 15:22:31 -0700

    4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -8,21 +8,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/',strict_slashes=False)
+
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
      returns Hello HBNB!
     """
     return 'Hello HBNB!'
 
-@app.route('/hbnb',strict_slashes=False)
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
      returns  HBNB!
     """
     return 'HBNB!'
 
-@app.route('/c/<text>',strict_slashes=False)
+
+@app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """
      returns  “C ” followed by the value of the text variable!
@@ -30,8 +33,9 @@ def cisfun(text):
     """
     return 'C ' + text.replace('_', ' ')
 
+
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
-@app.route('/python/<text>',strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def print_python(text):
     """
      returns  “python ” followed by the value of the text variable!
@@ -40,7 +44,7 @@ def print_python(text):
     return 'Python ' + text.replace('_', ' ')
 
 
-@app.route('/number/<int:n>',strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def print_n(n):
     """
       display “n is a number” only if n is an integer
