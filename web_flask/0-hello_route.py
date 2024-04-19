cff89d5a76642a6134ef0302bef6237e55c3b166
modidokeita <modibokeita9390gmail.com>
2024-04-19 15:11:13 -0700

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -11,7 +11,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/',strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
      returns Hello HBNB!
@@ -20,4 +20,4 @@ def hello_hbnb():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000') 
+    app.run(host='0.0.0.0', port='5000')
