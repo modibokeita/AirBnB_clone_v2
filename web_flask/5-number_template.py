fbbb0201238ae67e2170af64b9241cb08698c72f
modidokeita <modibokeita9390gmail.com>
2024-04-19 14:46:57 -0700

    5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -4,7 +4,7 @@
   script that starts a Flask web application
 """
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -48,7 +48,7 @@ def print_n(n):
     return "{:d} is a number".format(n)
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
-def numbersandtemplates(n):
+def display_templete(n):
     """display a HTML page only if n is an integer"""
     return render_template('5-number.html', n=n)
 
